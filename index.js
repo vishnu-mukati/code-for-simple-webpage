e4f66d6aba6e8d7b58fac5e7d5b2e2f0caaafedf
vishnu mukati <mukativishnu1232@gmail.com>
2024-04-09 18:43:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Write your code below:
+let list = document.getElementsByClassName('fruit');
+list[2].style.backgroundColor = 'yellow';
+for(let i=0;i<list.length;i++)
+  {
+    list[i].style.fontWeight ='bold'
+  }
