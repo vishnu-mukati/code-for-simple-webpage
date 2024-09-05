faf962b72e00fd36efbc3c83bea82d767cdda888
Vishnu Mukati <mukativishnu1232@gmail.com>
2024-09-05 20:22:02 +0530

    this is for the jsx

---
+++ OneDrive/Desktop/Learning React/project-1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+     <div>
+       <h1>Les't get started</h1>
+       <h1>Les't get started</h1>
+     </div>
+  );
+}
+
+export default App;
